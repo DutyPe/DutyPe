38e58db71c607d247c48a3c02447a3bec1a9d1ae
vamsib-298 <vamsib298@gmail.com>
2026-05-08 15:11:12 +0530

    Update referral reward copy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
 		applicationId = "com.dutype.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 71
-        versionName = "2.6.18"
+        versionCode = 72
+        versionName = "2.6.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
